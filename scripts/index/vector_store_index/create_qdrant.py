ba3e53059dda14fbed69b1c2f5b614fc94cc8f13
t-koyoyo <tanaka.koyoyo@gmail.com>
2023-07-29 10:15:16 +0900

    add: base files

--- scripts/index/vector_store_index/create_qdrant.py
+++ scripts/index/vector_store_index/create_qdrant.py
@@ -1,14 +1,46 @@
 import logging
 import sys
 import faiss
-from llama_index import SimpleDirectoryReader, StorageContext, VectorStoreIndex
-from llama_index.vector_stores.faiss import FaissVectorStore
+import qdrant_client
+
+from llama_index import ServiceContext, SimpleDirectoryReader, StorageContext, VectorStoreIndex
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+
+import custom_embed
 
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # ------------------------------
 # ■ Requirements
-#   - .env > OPENAI_API_KEY/OPENAI_API_HOST
 # https://gpt-index.readthedocs.io/en/v0.7.13/examples/vector_stores/QdrantIndexDemo.html
 # ------------------------------
+
+# ------------------------------
+# ■ Settings Constants
+# ------------------------------
+embed_model = custom_embed.embed_azure()  # Embedding Model
+client = qdrant_client.QdrantClient(path='../../../storages/vector_store_index/qdrant')
+
+# ------------------------------
+# ■ Load data
+# ------------------------------
+documents = SimpleDirectoryReader('../../../data').load_data()
+
+# ------------------------------
+# ■ Create index
+# ------------------------------
+vector_store = QdrantVectorStore(client=client, collection_name="my_collection")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+service_context = ServiceContext.from_defaults(embed_model=embed_model)
+index = VectorStoreIndex.from_documents(
+  documents=documents,
+  storage_context=storage_context,
+  service_context=service_context,
+  show_progress=True
+)
+
+# ------------------------------
+# ■ Save index
+# ------------------------------
+index.storage_context.persist('../../../storages/vector_store_index/qdrant')
